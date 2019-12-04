c9a45f12aae3ca26b80132777b23c0011892b2cc
pejak4 <53259486+pejak4@users.noreply.github.com>
2019-12-04 23:12:06 +0100

    3.15-1 Add reducer
    Who has list of patients

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import scheduleReducer from './store/reducer/schedule';
 import authReducer from './store/reducer/auth';
+import userReducer from './store/reducer/user';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
     schedule: scheduleReducer,
-    auth: authReducer
+    auth: authReducer,
+    user: userReducer
 });
 
 const store = createStore(rootReducer, composeEnhancers(
--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -4,3 +4,4 @@ export const ADD_EXAMINATION = 'ADD_EXAMINATION';
 export const SIGN_UP = 'SIGN_UP';
 export const LOG_IN = 'LOG_IN';
 export const CHANGE_PASSWORD = 'CHANGE_PASSOWRD';
+export const GET_PATIENTS = 'GET_PATIENTS';
