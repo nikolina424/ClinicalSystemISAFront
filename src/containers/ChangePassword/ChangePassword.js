b125a4c8ef52e276b662b850b518cf940881222d
pejak4 <53259486+pejak4@users.noreply.github.com>
2019-12-16 18:35:01 +0100

    Updated html and css for change password page

--- src/containers/ChangePassword/ChangePassword.js
+++ src/containers/ChangePassword/ChangePassword.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
-import classes from './ChangePassword.css';
+import './ChangePassword.css';
 import {updateObject} from '../../shared/utility';
+import axios from '../../axios-objects';
 
 class ChangePassword extends Component {
 
@@ -14,10 +15,32 @@ class ChangePassword extends Component {
         }
     }
 
-    changePassHandler = (event) => {
+    changePassHandler = async (event) => {
         event.preventDefault();
-        this.props.onChangePassword(this.state.auth.oldPass, this.state.auth.newPass, this.state.auth.newRepeatPass);
-        this.props.history.push('/');
+        const {oldPass, newPass, newRepeatPass} = this.state.auth;
+        const pass = {
+            oldPass, 
+            newPass,
+            newRepeatPass
+        };
+        const token = sessionStorage.getItem('token');
+
+        try {
+            const response = await axios.put('/changePassword', pass, {
+                headers: {
+                    'Authorization' : 'Bearer ' + token
+                }
+            });
+            if (response) {
+                this.props.history.push('/');
+                sessionStorage.setItem('firstTimeLogged', false);
+            }
+                
+        } catch(err) {
+            console.log(err);
+        }
+        // this.props.onChangePassword(this.state.auth.oldPass, this.state.auth.newPass, this.state.auth.newRepeatPass);
+        // this.props.history.push('/');
     }
 
     inputChangeHandler = (event, type) => {
@@ -30,17 +53,35 @@ class ChangePassword extends Component {
 
     render() {
         return (
-            <div>
-                <input type="password" className={classes.Input} 
-                    onChange={(event) => this.inputChangeHandler(event, 'oldPass')} />
-                <input type="password" className={classes.Input} 
-                    onChange={(event) => this.inputChangeHandler(event, 'newPass')} />
-                <input type="password" className={classes.Input} 
-                    onChange={(event) => this.inputChangeHandler(event, 'newRepeatPass')} />
-                <button className={classes.Button} 
-                    onClick={(event) => {this.changePassHandler(event)}}
-                >Save</button>
+            <div className="wrapper fadeInDown">
+                <div id="formContent">
+                    <form>
+                        <input type="password" className="fadeIn first" name="oldPassword" placeholder="Old password"
+                            onChange={(event) => this.inputChangeHandler(event, 'oldPass')}/>
+                        <input type="password" className="fadeIn second" name="newPassword" placeholder="New password"
+                            onChange={(event) => this.inputChangeHandler(event, 'newPass')}/>
+                        <input type="password" className="fadeIn third" name="repeatPassword" placeholder="Repeat new password"
+                            onChange={(event) => this.inputChangeHandler(event, 'newRepeatPass')}/>
+                        <input type="submit" className="fadeIn fourth" value="Change password" style={{cursor: 'pointer'}}
+                            onClick={(event) => this.changePassHandler(event)} />
+                    </form>
+                    <div id="formFooter">
+                        <a>Change your password for security reasons!</a>
+                    </div>
+                </div>
             </div>
+
+            // <div>
+            //     <input type="password" className={classes.Input} 
+            //         onChange={(event) => this.inputChangeHandler(event, 'oldPass')} />
+            //     <input type="password" className={classes.Input} 
+            //         onChange={(event) => this.inputChangeHandler(event, 'newPass')} />
+            //     <input type="password" className={classes.Input} 
+            //         onChange={(event) => this.inputChangeHandler(event, 'newRepeatPass')} />
+            //     <button className={classes.Button} 
+            //         onClick={(event) => {this.changePassHandler(event)}}
+            //     >Save</button>
+            // </div>
         );
     }
 }
