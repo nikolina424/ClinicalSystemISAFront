550f7191f55247f3005a662e13fe2fb4ec5ff773
pejak4 <53259486+pejak4@users.noreply.github.com>
2019-12-21 14:07:37 +0100

    Delete action classes from redux

--- src/store/actions/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const SCHEDULE_START = 'SCHEDULE_START';
-export const ADD_SURGERY = 'ADD_SURGERY';
-export const ADD_EXAMINATION = 'ADD_EXAMINATION';
-export const SIGN_UP = 'SIGN_UP';
-export const LOG_IN = 'LOG_IN';
-export const CHANGE_PASSWORD = 'CHANGE_PASSOWRD';
-export const GET_PATIENTS = 'GET_PATIENTS';
