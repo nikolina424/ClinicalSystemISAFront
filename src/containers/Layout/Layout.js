1cc417ea8a0835487afd1cdd755c89cd047f927b
Nikolina Ivankovic <inina007@gmail.com>
2020-01-26 11:52:11 +0100

    Update Layout.js
    Adding code for  homepage

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 import '../../bootstrap/bootstrap.css';
+import './Layout.css'
 
 class Layout extends React.PureComponent {
 
@@ -9,44 +10,56 @@ class Layout extends React.PureComponent {
         window.location.reload();
     }
 
+   
     render() {
         const token = sessionStorage.getItem('token');
         const role = sessionStorage.getItem('role');
 
         return (
+           <Auxiliary>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-                <div className="container">
-                    <a className="navbar-brand" 
-                    href="/">Home page</a>
-                    {token === null ? 
-                        <Auxiliary>
-                            <a className="navbar-brand" 
-                            href="/login">Sign in</a>
-                            <a className="navbar-brand" 
-                            href="/register">Sign up</a>
-                        </Auxiliary>
-                    : null }
-                    {(token !== null && role === 'DOCTOR') ?
-                        <Auxiliary>
-                            <a className="navbar-brand" 
-                            href="/schedule">Schedule</a>
-                        </Auxiliary> 
-                    : null }
-                    {token !== null ? 
-                        <Auxiliary>
-                            <a className="navbar-brand" 
-                            href="/profile">Profile</a>
-                        </Auxiliary>
-                    : null }
-                    {token !== null ?
-                        <Auxiliary>
-                            <a className="navbar-brand" style={{cursor: 'pointer'}}
-                            onClick={this.logoutHandler}>Logout</a>
-                        </Auxiliary> 
-                    : null}
-                </div>
-            </nav>
-        );
+                    <div className="container">
+                        <a className="navbar-brand" 
+                        href="/">Home page</a>
+                      {token === null ? 
+                            <Auxiliary>
+                                <a className="navbar-brand" 
+                                href="/login">Sign in</a>
+                                <a className="navbar-brand" 
+                                href="/register">Sign up</a>
+                            </Auxiliary>
+                        : null }
+                        {(token !== null && role === 'DOCTOR') ?
+                            <Auxiliary>
+                                <a className="navbar-brand" 
+                                href="/schedule">Schedule</a>
+                            </Auxiliary> 
+                        : null }
+                        {token !== null ? 
+                            <Auxiliary>
+                                <a className="navbar-brand" 
+                                href="/profile">Profile</a>
+                            </Auxiliary>
+                        : null }
+                        {token !== null ?
+                            <Auxiliary>
+                                <a className="navbar-brand" style={{cursor: 'pointer'}}
+                                onClick={this.logoutHandler}>Logout</a>
+                            </Auxiliary> 
+                        : null}
+                    </div>
+                </nav>
+                
+                <img alt="doctors" className={`doctor`} src="../images/doctors.jpg"  />
+
+                <p className={`p`} >
+                    Welcome!
+                   <br></br>
+                    We are here to promote and improve the quality of life by providing integrated healthcare services to the entire community. Feel free to join us!
+                </p>
+
+            </Auxiliary>
+);
     }
 }
 
