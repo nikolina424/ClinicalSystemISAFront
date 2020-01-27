a846e3f665c4d2f338e6259b2f3b8f0e6574f364
Nikolina Ivankovic <inina007@gmail.com>
2020-01-27 15:55:30 +0100

    Update AdminPage.js
    Adding some react code to modify the page

--- src/components/Admin/AdminPage/AdminPage.js
+++ src/components/Admin/AdminPage/AdminPage.js
@@ -3,6 +3,7 @@ import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import Navigation from '../../../containers/Navigation/Navigation';
 import axios from '../../../axios-objects';
 import {updateObject} from '../../../shared/utility';
+import '../../../bootstrap/bootstrap.css'
 
 class AdminPage extends React.PureComponent {
 
@@ -112,53 +113,65 @@ class AdminPage extends React.PureComponent {
         return (
             <Auxiliary>
                 <Navigation />
-                <div>
-                    <p>Number of clinic center admins: {this.state.ccadmins.length} </p>
-                    {this.state.ccadmins.map((ad, i) => {
-                        return (
-                            <div key={i}>
-                                <div className="ui card">
-                                    <div className="image">
-                                        {ad.image !== null ? <img src={'images/' + ad.image} alt="slika" style={{width: '40px', height: '40px'}}/> : null }
-                                    </div>
-                                    <div className="content">
-                                        <p className="header">{ad.firstName} {ad.lastName}</p>
-                                        <div className="meta">
-                                        <span className="date">Joined in 2020</span>
+                <div className="d1" style={{width:'100%', height:'auto', display:'flex', flexDirection:'row'}}>
+                    <div style={{float:'left', height:'100vw', width:'50%'}}>
+                        <p style={{textAlign:'center'}}>Number of clinic center admins: {this.state.ccadmins.length} </p>
+                        {this.state.ccadmins.map((ad, i) => {
+                            return (
+                                <div key={i} >
+                                    <div className="ui card mx-auto"  style={{display:'block' }}>
+                                        <div className="image">
+                                            {ad.image !== null ? <img src={'images/' + ad.image} alt="slika" style={{width: '40px', height: '40px'}}/> : null }
                                         </div>
-                                        <div className="description">
-                                            {ad.phoneNumber}
+                                        <div className="content" >
+                                            <p className="header" >{ad.firstName} {ad.lastName}</p>
+                                            <div className="meta">
+                                            <span className="date">Joined in 2020</span>
+                                            </div>
+                                            <div className="description">
+                                                {ad.phoneNumber}
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="extra content">
-                                        {ad.id === this.state.loggedUser.user.id ? <a href="/profile">
-                                        <i className="user icon"></i>
-                                            See your profile
-                                        </a> : null }
-                                    </div>
-                                    </div>
-                            </div>
-                        );
-                    })}
-                    <button onClick={(event) => this.showFormHandler(event)}>Add new Clinic Center Admin</button>
-                    {this.state.openList ? 
-                        <Auxiliary>
-                            {(this.state.loggedUser.user.role === 'ADMINCC' && this.state.loggedUser.user.predefined) ?
+                                        <div className="extra content">
+                                            {ad.id === this.state.loggedUser.user.id ? <a href="/profile">
+                                            <i className="user icon"></i>
+                                                See your profile
+                                            </a> : null }
+                                        </div>
+                                        </div>
+                                </div>
+                            );
+                        })}
+                        <br></br>
+                        <button className="mx-auto" style={{display:'block'}} onClick={(event) => this.showFormHandler(event)} >Add new Clinic Center Admin</button>
+                        {this.state.openList ? 
                             <Auxiliary>
-                                <input type="text" placeholder="First Name" onChange={(event) => this.inputChangeHandler(event, 'firstName')}/> 
-                                <input type="text" placeholder="Last Name" onChange={(event) => this.inputChangeHandler(event, 'lastName')}/>
-                                <input type="text" placeholder="Email" onChange={(event) => this.inputChangeHandler(event, 'email')}/> 
-                                <input type="password" placeholder="Password" onChange={(event) => this.inputChangeHandler(event, 'password')}/> 
-                                <input type="text" placeholder="Address" onChange={(event) => this.inputChangeHandler(event, 'address')}/> 
-                                <input type="text" placeholder="City" onChange={(event) => this.inputChangeHandler(event, 'city')}/>
-                                <input type="text" placeholder="Country" onChange={(event) => this.inputChangeHandler(event, 'country')}/>  
-                                <input type="number" placeholder="Phone Number" onChange={(event) => this.inputChangeHandler(event, 'phoneNumber')}/>
-                                <input type="number" placeholder="User ID" onChange={(event) => this.inputChangeHandler(event, 'userId')}/>  
-                                <button onClick={(event) => this.addNewClinicCenterAdmin(event)}>Add</button>
+                                {(this.state.loggedUser.user.role === 'ADMINCC' && this.state.loggedUser.user.predefined) ?
+                                <div className="registration  mx-auto" style={{width:'300px', display:'block'}}>
+                                    <Auxiliary>
+                                        <input type="text" className="mx-auto" style={{display:'block'}}  placeholder="First Name" onChange={(event) => this.inputChangeHandler(event, 'firstName')}/> 
+                                        <input type="text"  className="mx-auto" style={{display:'block'}} placeholder="Last Name" onChange={(event) => this.inputChangeHandler(event, 'lastName')}/>
+                                        <input type="text"  className="mx-auto" style={{display:'block'}} placeholder="Email" onChange={(event) => this.inputChangeHandler(event, 'email')}/> 
+                                        <input type="password"  className="mx-auto" style={{display:'block'}} placeholder="Password" onChange={(event) => this.inputChangeHandler(event, 'password')}/> 
+                                        <input type="text"  className="mx-auto" style={{display:'block'}} placeholder="Address" onChange={(event) => this.inputChangeHandler(event, 'address')}/> 
+                                        <input type="text"  className="mx-auto" style={{display:'block'}} placeholder="City" onChange={(event) => this.inputChangeHandler(event, 'city')}/>
+                                        <input type="text" className="mx-auto" style={{display:'block'}}  placeholder="Country" onChange={(event) => this.inputChangeHandler(event, 'country')}/>  
+                                        <br></br>
+                                        <input type="number"  className="mx-auto" style={{display:'block'}} placeholder="Phone Number" onChange={(event) => this.inputChangeHandler(event, 'phoneNumber')} />
+                                        <br></br>
+                                        <input type="number" className="mx-auto" style={{display:'block'}}  placeholder="User ID" onChange={(event) => this.inputChangeHandler(event, 'userId')} />  
+                                        <br></br>
+                                        <button  className="mx-auto" style={{display:'block'}} onClick={(event) => this.addNewClinicCenterAdmin(event)  }>Add</button>
+                                    </Auxiliary>
+                                </div>
+                                    : null }
                             </Auxiliary>
-                            : null }
-                        </Auxiliary>
-                    : null}
+                        : null}
+                    </div>
+
+                    <div style={{float:'right', height:'100vw', width:'50%'}}>
+                        
+                    </div>
                 </div>
             </Auxiliary>
         );
